a91ab7bb42168a9991cd2f058b5f11ace622f4f6
saddamhussain73 <164578060+saddamhussain73@users.noreply.github.com>
2024-11-10 07:16:14 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,58 @@
-import Image from "next/image";
+
+import Link from "next/link"
+import Navbar from "./components/Navbar"
+import Herosection from "./components/Herosection"
+import Footer from "./components/Footer"
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  return(
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+<div>
+  
+<Herosection />
+<div className="bg-slate-100 py-8 px-10 dark:bg-slate-700">
+  <div className=" flex justify-between">
+    <h1 className=" font-bold text-2xl text-black">Featured New Cars</h1>
+    <div className=" text-blue-700 text-sm font-medium hover:text-red-700">View All New Cars</div>
+  </div>
+  <ul className="flex font-semibold py-6 space-x-10 text-black">
+    <li className=" hover:text-red-700">Popular</li>
+    <li className=" hover:text-red-700">Upcoming</li>
+    <li className=" hover:text-red-700">Newly Launched</li>
+  </ul>
+
+  <div className="flex space-x-6 text-center ">
+    <ul className=" w-1/4 bg-white text-center">
+      <Link href="/alto_details"><li className=" hover:opacity-50 trasition duration-300"><img src="https://cache4.pakwheels.com/system/car_generation_pictures/6013/medium/Suzuki_Alto_-_PNG.png?1635945100" alt="sorry" /></li></Link>
+      <Link href="/alto_details"><li className="text-blue-900 font-semibold hover:text-red-700">Suzuki Alto</li></Link>
+      <Link href="/alto_details"><li className="text-green-500 font-semibold hover:text-red-700">PKR 25.5 - 32.5 lacs</li></Link>
+    </ul>
+
+    <ul className=" w-1/4 bg-white text-center">
+      <Link href="/city_details"><li className=" hover:opacity-50 trasition duration-300"><img src="https://cache1.pakwheels.com/system/car_generation_pictures/6425/medium/Honda_City_Front.jpg?1651424945" alt="sorry" /></li></Link>
+      <Link href="/city_details"><li className="text-blue-900 font-semibold hover:text-red-700">Honda City</li></Link>
+      <Link href="/city_details"><li className="text-green-500 font-semibold hover:text-red-700">PKR 25.5 - 32.5 lacs</li></Link>
+    </ul>
+
+    <ul className=" w-1/4 bg-white text-center">
+      <Link href="/corolla_details"><li className=" hover:opacity-50 trasition duration-300"><img src="https://cache1.pakwheels.com/system/car_generation_pictures/5361/medium/Corolla-X-Cars-Cropped-Pictures-for-Website.jpg?1606903674" alt="sorry" /></li></Link>
+      <Link href="/corolla_details"><li className="text-blue-900 font-semibold hover:text-red-700">Toyota Corolla</li></Link>
+      <Link href="/corolla_details"><li className="text-green-500 font-semibold hover:text-red-700">PKR 25.5 - 32.5 lacs</li></Link>
+    </ul>
+
+    <ul className=" w-1/4 bg-white text-center">
+      <Link href="/honda_details"><li className=" hover:opacity-50 trasition duration-300"><img src="https://cache4.pakwheels.com/system/car_generation_pictures/7370/medium/Cover.jpg?1677570254" alt="sorry" /></li></Link>
+      <Link href="/honda_details"><li className="text-blue-900 font-semibold hover:text-red-700">Honda Civic</li></Link>
+      <Link href="/honda_details"><li className="text-green-500 font-semibold hover:text-red-700">PKR 25.5 - 32.5 lacs</li></Link>
+    </ul>
+  </div>
+  </div>
+</div>
+
+
+
+
+  )
+
 }
